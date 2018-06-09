53a28bec03725fd9f932137426133e5ecc0ebfc7
danie <danie@DanielLaptop.fritz.box>
2018-06-09 22:29:10 +0200

    test

--- Database.js
+++ Database.js
@@ -43,7 +43,7 @@ function findAll(_callback) {
                 line += studentArray[i].gender ? "(M)" : "(F)";
                 line += "\n";
             }
-            _callback(JSON.stringify(line));
+            _callback(line);
         }
     }
 }
--- Database.ts
+++ Database.ts
@@ -49,7 +49,7 @@ export function findAll(_callback: Function): void {
                 line += studentArray[i].gender ? "(M)" : "(F)";
                 line += "\n";
             }
-            _callback(JSON.stringify(line));
+            _callback(line);
         }
     }
 }
