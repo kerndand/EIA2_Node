b0ad4f259da594bc050aed6811f79123a2bac0ad
danie <danie@DanielLaptop.fritz.box>
2018-06-09 17:09:41 +0200

    test

--- Database.ts
+++ Database.ts
@@ -43,13 +43,13 @@ export function findAll(_callback: Function): void {
         if (_e) {
             _callback("Error" + _e);
         } else {
-            let line: string;
-            for (let i: number = 0; i < studentArray.length; i++) {
-            line += studentArray[i].matrikel + ": " + studentArray[i].studiengang + ", " + studentArray[i].name + ", " + studentArray[i].firstname + ", " + studentArray[i].age + ", "; 
-            line += studentArray[i].gender ? "(M)" : "(F)";
-            line += "\n";
-            }
-            _callback(line, null);
+//            let line: string;
+//            for (let i: number = 0; i < studentArray.length; i++) {
+//            line += studentArray[i].matrikel + ": " + studentArray[i].studiengang + ", " + studentArray[i].name + ", " + studentArray[i].firstname + ", " + studentArray[i].age + ", "; 
+//            line += studentArray[i].gender ? "(M)" : "(F)";
+//            line += "\n";
+//            }
+            _callback(JSON.stringify(studentArray));
         }
     }
 }
